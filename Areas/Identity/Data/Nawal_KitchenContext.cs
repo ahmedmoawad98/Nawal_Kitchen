0698d6f012a4651ce0f960c452115da25d82ace2
Ahmed Moawad <ahmedmoawad9823@gmail.com>
2022-01-22 13:34:40 -0500

    first chekin

---
+++ Areas/Identity/Data/Nawal_KitchenContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Nawal_Kitchen.Models;
+
+namespace Nawal_Kitchen.Data
+{
+    public class Nawal_KitchenContext : IdentityDbContext<IdentityUser>
+    {
+        public Nawal_KitchenContext(DbContextOptions<Nawal_KitchenContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<ApplicationType> ApplicationType { get; set; }
+        public DbSet<Product> Product { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nawal_Kitchen.Data;
+
+[assembly: HostingStartup(typeof(Nawal_Kitchen.Areas.Identity.IdentityHostingStartup))]
+namespace Nawal_Kitchen.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<Nawal_KitchenContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("Nawal_KitchenContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<Nawal_KitchenContext>();
+            });
+        }
+    }
+}
